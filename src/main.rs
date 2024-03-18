631e516e683c063aa70e6d930f5bf2a1419016b5
Gabriel Nogueira <gabriel.nogueira21@outlook.com>
2024-03-18 18:54:58 -0300

    Done

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,14 @@ use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+
     let config = Config::build(&args).unwrap_or_else(|err|{
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    println!("{:?}", config.path());
+
+    if let Err(e) = ls_clone::run(config) {
+        eprintln!("{e}");
+        process::exit(1);
+    }
 }
