234000a95a60cf2461773cb9a8a53437329bb967
Gabriel Nogueira <gabriel.nogueira21@outlook.com>
2024-03-18 18:54:53 -0300

    Get dir files and print to console

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{env, error::Error, path::PathBuf, process};
+use std::{env, error::Error, fs, path::PathBuf, process};
 
 pub struct Config {
     file_path: PathBuf,
@@ -7,17 +7,12 @@ pub struct Config {
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, Box<dyn Error>> {
         let file_path: PathBuf = match args.len() {
-        
-            1 => {
-                env::current_dir()?
-            }
+            1 => env::current_dir()?,
 
-            2 => {
-                PathBuf::from(args[1].clone())
-            }
+            2 => PathBuf::from(args[1].clone()),
 
             _ => {
-                eprintln!("Too much arguments!\nusage: ls <path> ");
+                eprintln!("\nusage: ls <path> ");
                 process::exit(1);
             }
         };
@@ -29,4 +24,21 @@ impl Config {
     }
 }
 
+pub fn get_dir_files(path: PathBuf) -> Result<Vec<String>, Box<dyn Error>> {
+    let mut files = Vec::new();
+    let dir = fs::read_dir(path)?;
+    for file in dir {
+        files.push(file?.file_name().to_str().unwrap().to_string());
+    }
 
+    Ok(files)
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let file_list = get_dir_files(config.path())?;
+    for file in file_list {
+        println!("{}", file)
+    }
+
+    Ok(())
+}
